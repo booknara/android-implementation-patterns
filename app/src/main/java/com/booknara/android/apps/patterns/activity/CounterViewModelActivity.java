63ec809677b8fd912b08862776c648f697e31564
Daehee Han <bookdori81@gmail.com>
2020-11-20 00:54:21 -0800

    Update AndroidX KTX example

--- app/src/main/java/com/booknara/android/apps/patterns/activity/CounterViewModelActivity.java
+++ app/src/main/java/com/booknara/android/apps/patterns/activity/CounterViewModelActivity.java
@@ -2,46 +2,40 @@ package com.booknara.android.apps.patterns.activity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.booknara.android.apps.patterns.R;
+import com.booknara.android.apps.patterns.databinding.ActivityCounterBinding;
 import com.booknara.android.apps.patterns.utils.LoggingInterceptor;
 import com.booknara.android.apps.patterns.viewmodel.CounterViewModel;
 import com.booknara.android.apps.patterns.viewmodel.factory.CounterViewModelFactory;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
 public class CounterViewModelActivity extends FragmentActivity {
 
-    @BindView(R.id.number_text)
-    protected TextView numberText;
-
+    private ActivityCounterBinding binding;
     private CounterViewModel viewModel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_counter);
-
-        ButterKnife.bind(this);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_counter);
+        binding.incrementalButton.setOnClickListener(this::incrementClickCount);
 
         CounterViewModelFactory counterViewModelFactory = new CounterViewModelFactory(new LoggingInterceptor());
         viewModel = ViewModelProviders.of(this, counterViewModelFactory).get(CounterViewModel.class);
         displayClickCount(viewModel.getCount());
     }
 
-    @OnClick(R.id.incremental_button)
     public void incrementClickCount(View button) {
-        int number = Integer.parseInt(numberText.getText().toString());
+        int number = Integer.parseInt(binding.numberText.getText().toString());
         displayClickCount(++number);
     }
 
     private void displayClickCount(int counter) {
-        numberText.setText(String.valueOf(counter));
+        binding.numberText.setText(String.valueOf(counter));
     }
 }
---
+++ app/src/main/java/com/booknara/android/apps/patterns/activity/SimpleFragmentActivity.kt
@@ -0,0 +1,50 @@
+package com.booknara.android.apps.patterns.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.booknara.android.apps.patterns.Constants.IntentKey.ACTIVITY_TITLE
+import com.booknara.android.apps.patterns.R
+import com.booknara.android.apps.patterns.databinding.ActivitySimpleFragmentBinding
+import com.booknara.android.apps.patterns.fragment.SimpleFragment
+
+class SimpleFragmentActivity: AppCompatActivity() {
+
+    lateinit var binding: ActivitySimpleFragmentBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_simple_fragment)
+
+        var title = ""
+        intent?.let {
+            title = it.getStringExtra(ACTIVITY_TITLE)
+        }
+        binding.toolbarView.title = title
+        setSupportActionBar(binding.toolbarView)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        setDetailContainer(SimpleFragment.newInstance())
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun setDetailContainer(fragment: Fragment) {
+//        val ft = supportFragmentManager.beginTransaction()
+//        ft.add(R.id.simple_fragment, fragment, SimpleFragment.TAG)
+//        ft.commitAllowingStateLoss()
+
+        supportFragmentManager.commit(allowStateLoss = true) {
+            add(R.id.simple_fragment, fragment, SimpleFragment.TAG)
+        }
+    }
+
+    companion object {
+        const val TAG = "SimpleFragmentActivity"
+    }
+}
